7d435daccdb3513326d183a7a7e80d7193d262c2
Cassiano de Souza <redhotcassiano@gmail.com>
2018-01-09 17:19:11 -0200

    Integração com a camera.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [
+    Camera
+  ]
 })
 export class HomePage {
+  img = "";
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  tirarFoto(){
+    const options: CameraOptions = {
+      quality: 10,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
   }
 
 }
